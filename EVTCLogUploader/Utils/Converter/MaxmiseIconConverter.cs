d88f6f3ce2b19544d38d4e3bace297649ad9941b
Henry Timms <36480544+Hen676@users.noreply.github.com>
2024-03-07 18:08:43 +0000

    UI Improvments

---
+++ EVTCLogUploader/Utils/Converter/MaxmiseIconConverter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace EVTCLogUploader.Utils.Converter
+{
+    internal class MaxmiseIconConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is WindowState windowState)
+            {
+                return (windowState == WindowState.FullScreen || windowState == WindowState.Maximized) ? "🗗" : "🗖";
+            }
+            // converter used for the wrong type
+            return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EVTCLogUploader/ViewModels/ViewModelBase.cs
+++ EVTCLogUploader/ViewModels/ViewModelBase.cs
@@ -7,12 +7,18 @@ namespace EVTCLogUploader.ViewModels
     public class ViewModelBase : ReactiveObject
     {
         public ReactiveCommand<Window, Unit> CloseCommand { get; }
-
-        public void Close(Window window) => window.Close();
+        public ReactiveCommand<Window, Unit> MinimizeCommand { get; }
+        public ReactiveCommand<Window, Unit> MaximizeCommand { get; }
 
         public ViewModelBase()
         {
             CloseCommand = ReactiveCommand.Create<Window>(Close);
+            MinimizeCommand = ReactiveCommand.Create<Window>(Minimize);
+            MaximizeCommand = ReactiveCommand.Create<Window>(Maximize);
         }
+
+        private void Maximize(Window window) => window.WindowState = (window.WindowState == WindowState.FullScreen || window.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        private void Minimize(Window window) => window.WindowState = WindowState.Minimized;
+        private void Close(Window window) => window.Close();
     }
 }
